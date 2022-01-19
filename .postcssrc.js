a4b411af72cb791adcf842e954eac06cceedeac3
Hyeonjun <sty110357@gmail.com>
2022-01-19 10:39:20 +0000

    [INIT]   initializing

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommonJS
+
+// import autoprefixer form 'autoprefixer';
+const autoprefixer = require('autoprefixer');
+
+// export {
+//   plugins: [
+//       autoprefixer
+//   ]
+// }
+module.exports = {
+    plugins: [
+        autoprefixer
+    ]
+}
